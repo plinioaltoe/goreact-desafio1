e1e3b2bda243903fb0cabedb5febd6645d4594a0
plinioaltoe <plinioaltoe@yahoo.com.br>
2019-03-10 16:32:30 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React, { Component, Fragment } from 'react';
+import { render } from 'react-dom';
+import Button from './Button';
+import './style.scss';
+
+class App extends Component {
+  state = {
+    counter: 0,
+  }
+
+  handleClick = () => {
+    const { counter } = this.state;
+    this.setState({ counter: counter + 1 });
+  }
+
+  render() {
+    const { counter } = this.state;
+    return (
+      <Fragment>
+        <h1>Hello World 2000</h1>
+        <h2>{counter}</h2>
+        <Button onClick={this.handleClick}>Enviar</Button>
+      </Fragment>
+    );
+  }
+}
+
+render(<App />, document.getElementById('app'));
